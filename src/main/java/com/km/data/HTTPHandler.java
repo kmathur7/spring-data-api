25fae8b3f0c67f12e418e30f2564c49e3fcb825b
Kunal Mathur <mathurkunal@gmail.com>
2020-04-18 01:03:34 -0400

    adding API, Swagger UI and Maven pluging

---
+++ src/main/java/com/km/data/HTTPHandler.java
@@ -0,0 +1,16 @@
+package com.km.data;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/info")
+public class HTTPHandler {
+
+  @GetMapping("/{id}")
+  public String findById(@PathVariable String id) {
+    return "Received " + id;
+  }
+}
